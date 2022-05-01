0a579b74a8b308f69547b004e2cf5b6a94fb9fa5
Brian Holt <btholt@gmail.com>
2022-05-01 12:24:29 -0700

    wordmasters

--- lessons/06-talking-to-servers/D-project.md
+++ lessons/06-talking-to-servers/D-project.md
@@ -17,15 +17,114 @@ You and I are going to recreate the well known word game Wordle. If you're not f
 
 Above you have a working copy of it, feel free to try (you may need to click on it.)
 
-One constaint that normal Wordle has that _we are not_ going to do is that Wordle only lets you guess valid words. We'll skip that requirement in the name of simplicity.
+One constaint that normal Wordle has that _we are not_ going to do is that Wordle only lets you guess valid words. We'll skip that requirement in the name of simplicity. That is a stretch goal for you.
 
 Moreover, you need to call an API to get the secret word. Frontend Masters has created one that you can call here:
 
-# TODO: put in API call documentation
+## The API
 
-You will need to do all the HTML, CSS, and JavaScript yourself. There are infinite goood and correct ways to do this so don't feel like you need to match my solution.
+You have two APIs to work with today:
 
-For reference, this took me about 100 lines of JavaScript, 50 lines of HTML, and 50 lines of CSS.
+**GET http://localhost:3000/get-word-of-the-day**
+
+- This will give you the word of the day. It changes every night at midnight
+- The response will look like this: `{"word":"humph","puzzleNumber":3}` where the word is the current word of the day and the puzzleNumber is which puzzle of the day it is
+- If you do add `random=1` to the end of your URL (127.0.0.1:3000/get-word-of-the-day?random=1) then it will give you a random word of the day, not just the same daily one.
+
+**POST http://localhost:3000/validate-word**
+
+- You must _POST_ to this endpoint, not GET.
+- The endpoint expects JSON with a property called `"word"`. A valid post body would be `{ "word": "crane" }`.
+- The API will return back to you the word you sent and `isValid` which will be true or false. e.g. `{ "word": "crane", "isValid": true }` or `{ "word": "asdfg", "isValid": false }`.
+- This endpoint only validates five letter words.
+- This endpoint only validates English words and will not validate any accents or non-letter characters
+- Do this part last. It's the hardest part to get right.
+
+## Tips and Tricks
+
+- Solve one problem at the time. Maybe create and style the HTML and CSS first, then just make a user able to guess six times. Then add detection for correct guesses. Then add highlighting in green correct letters. Then add gray for definitely wrong letters. Then do yellow for close.
+- I didn't show you all the JavaScript methods you're going to need to do this. **You will need to Google things**. Learning how to answer your own questions is perhaps the most useful you'll learn in this course.
+- I added some animations, you don't have to.
+
+## Quick animation primer
+
+```html
+<style>
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  .spinny-boi {
+    animation: spin 0.5s infinite linear;
+    display: inline-block;
+    font-size: 30px;
+  }
+</style>
+<div class="spinny-boi">🤢</div>
+```
+
+- The `@keyframes` part allows you to define a reusable animation. We could throw `spin` on anything now.
+- We just definte `to` which is what you want the end state of your animation state to be. We could define `from` too which is where you want your animation to start from. Implicitly if you don't define a from, your animation starts from where it is already. In this case, the `spinny-boi` isn't rotated at all, or `transform: rotate(0deg)`. So it will rotate from 0 t0 360, or a full revolution. Feel free to play with it.
+- `animation` is the property to _use_ a defined keyframe. Here we just say "do what spin defines, the whole animation should take a half second, do it infinitely (aka never stop), and do it in a linear fashion (so there's no speed-up or speed-down in the animation). This is a short hand. You can define the properties separately. [See the MDN here][mdn].
+
+```html
+<style>
+  @keyframes rainbow {
+    100%,
+    0% {
+      color: rgb(255, 0, 0);
+    }
+    8% {
+      color: rgb(255, 127, 0);
+    }
+    16% {
+      color: rgb(255, 255, 0);
+    }
+    25% {
+      color: rgb(127, 255, 0);
+    }
+    33% {
+      color: rgb(0, 255, 0);
+    }
+    41% {
+      color: rgb(0, 255, 127);
+    }
+    50% {
+      color: rgb(0, 255, 255);
+    }
+    58% {
+      color: rgb(0, 127, 255);
+    }
+    66% {
+      color: rgb(0, 0, 255);
+    }
+    75% {
+      color: rgb(127, 0, 255);
+    }
+    83% {
+      color: rgb(255, 0, 255);
+    }
+    91% {
+      color: rgb(255, 0, 127);
+    }
+  }
+  .rainbow-boi {
+    animation: rainbow 4s infinite linear;
+    font-size: 30px;
+  }
+</style>
+<div class="rainbow-boi">Rainbow</div>
+```
+
+- Similar idea but with color. Lots of things can be animated, like size and position.
+- `from` is an alias for `0%` and `to` is an alias for `100%`. You can define percentages like we did here.
+
+## Get to it!
+
+You will need to do all the HTML, CSS, and JavaScript yourself. There are infinite good and correct ways to do this so don't feel like you need to match my solution.
+
+For reference, this took me about 125 lines of JavaScript, 50 lines of HTML, and 75 lines of CSS.
 
 This is a hard one! Give it your best shot and tweet me the results!
 
@@ -36,3 +135,4 @@ This is a hard one! Give it your best shot and tweet me the results!
 [html]: https://btholt.github.io/complete-intro-to-web-dev-v3/project-files/word-masters.html
 [css]: https://btholt.github.io/complete-intro-to-web-dev-v3/project-files/word-masters.css
 [js]: https://btholt.github.io/complete-intro-to-web-dev-v3/project-files/word-masters.js
+[mdn]: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Using_CSS_animations
--- public/project-files/word-masters.js
+++ public/project-files/word-masters.js
@@ -1,22 +1,27 @@
 const ANSWER_LENGTH = 5;
 const ROUNDS = 6;
 const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelector(".info-bar");
 
+// I like to do an init function so I can use "await"
 async function init() {
+  // the state for the app
   let currentRow = 0;
   let currentGuess = "";
   let done = false;
-  let isShowingModal = true;
+  let isLoading = true;
 
+  // nab the word of the day
   const res = await fetch(
     "http://127.0.0.1:3000/get-word-of-the-day?random=true"
   );
   const { word: wordRes } = await res.json();
   const word = wordRes.toUpperCase();
   const wordParts = word.split("");
-  isShowingModal = false;
-  setLoading(isShowingModal);
+  isLoading = false;
+  setLoading(isLoading);
 
+  // user adds a letter to the current guess
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       currentGuess += letter;
@@ -28,32 +33,37 @@ async function init() {
       letter;
   }
 
+  // use tries to enter a guess
   async function commit() {
     if (currentGuess.length !== ANSWER_LENGTH) {
       // do nothing
       return;
     }
 
-    isShowingModal = true;
-    setLoading(isShowingModal);
+    // check the API to see if it's a valid word
+    // skip this step if you're not checking for valid words
+    isLoading = true;
+    setLoading(isLoading);
     const res = await fetch("http://localhost:3000/validate-word", {
       method: "POST",
       body: JSON.stringify({ word: currentGuess }),
     });
     const { isValidWord } = await res.json();
+    isLoading = false;
+    setLoading(isLoading);
 
+    // not valid, mark the word as invalid and return
     if (!isValidWord) {
-      setLoading(false);
-      setInfo("Not a valid word", isShowingModal);
+      markInvalidWord();
       return;
     }
-    isShowingModal = false;
-    setLoading(isShowingModal);
 
     const guessParts = currentGuess.split("");
     const map = makeMap(wordParts);
     let allRight = true;
 
+    // first pass just finds correct letters so we can mark those as
+    // correct first
     for (let i = 0; i < ANSWER_LENGTH; i++) {
       if (guessParts[i] === wordParts[i]) {
         // mark as correct
@@ -62,6 +72,9 @@ async function init() {
       }
     }
 
+    // second pass finds close and wrong letters
+    // we use the map to make sure we mark the correct amount of
+    // close letters
     for (let i = 0; i < ANSWER_LENGTH; i++) {
       if (guessParts[i] === wordParts[i]) {
         // do nothing
@@ -81,24 +94,40 @@ async function init() {
     currentGuess = "";
     if (allRight) {
       // win
-      isShowingModal = true;
-      setInfo("you win", isShowingModal);
+      alert("you win");
       done = true;
     } else if (currentRow === ROUNDS) {
       // lose
-      isShowingModal = true;
-      setInfo(`you lose, the word was ${word}`, isShowingModal);
+      alert(`you lose, the word was ${word}`);
       done = true;
     }
   }
 
+  // user hits backspace, if the the length of the string is 0 then do
+  // nothing
   function backspace() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
     letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = "";
   }
 
+  // let the user know that their guess wasn't a real word
+  // skip this if you're not doing guess validation
+  function markInvalidWord() {
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
+
+      // long enough for the browser to repaint without the "invalid class" so we can then add it again
+      setTimeout(
+        () => letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid"),
+        10
+      );
+    }
+  }
+
+  // listening for event keys and routing to the right function
+  // we listen on keydown so we can catch Enter and Backspace
   document.addEventListener("keydown", function handleKeyPress(event) {
-    if (done || isShowingModal) {
+    if (done || isLoading) {
       // do nothing;
       return;
     }
@@ -115,33 +144,24 @@ async function init() {
       // do nothing
     }
   });
-
-  document
-    .querySelector(".info-button")
-    .addEventListener("click", function handleInfoButtonClick() {
-      isShowingModal = false;
-      setInfo("", isShowingModal);
-    });
 }
 
+// a little function to check to see if a character is alphabet letter
+// this uses regex (the /[a-zA-Z]/ part) but don't worry about it
+// you can learn that later and don't need it too frequently
 function isLetter(letter) {
   return letter.length === 1 && letter.match(/[a-zA-Z]/) !== null;
 }
 
-const loadingDiv = document.querySelector(".loading");
-const infoDiv = document.querySelector(".info");
-const infoTextDiv = document.querySelector(".info-text");
-const spiralDiv = document.querySelector(".spiral");
-function setLoading(isShowingModal) {
-  loadingDiv.classList.toggle("hidden", !isShowingModal);
-  spiralDiv.classList.toggle("hidden", !isShowingModal);
-}
-function setInfo(text, isShowingModal) {
-  loadingDiv.classList.toggle("hidden", !isShowingModal);
-  infoDiv.classList.toggle("hidden", !isShowingModal);
-  infoTextDiv.innerText = text;
+// show the loading spinner when needed
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle("hidden", !isLoading);
 }
 
+// takes an array of letters (like ['E', 'L', 'I', 'T', 'E']) and creates
+// an object out of it (like {E: 2, L: 1, T: 1}) so we can use that to
+// make sure we get the correct amount of letters marked close instead
+// of just wrong or correct
 function makeMap(array) {
   const obj = {};
   for (let i = 0; i < array.length; i++) {
