21e08ecf4e946a86b0b0b438203b627aa1495df8
Brian Holt <btholt@gmail.com>
2022-04-30 18:53:17 -0700

    added word validation to word masters

--- public/project-files/word-masters.js
+++ public/project-files/word-masters.js
@@ -1,24 +1,21 @@
 const ANSWER_LENGTH = 5;
 const ROUNDS = 6;
-const word = [
-  "CRANE",
-  "LOLLY",
-  "OXIDE",
-  "FEARS",
-  "PROXY",
-  "KNOLL",
-  "ELDER",
-  "CAULK",
-  "SHAKE",
-  "ABBEY",
-][Math.floor(Math.random() * 10)];
-const wordParts = word.split("");
 const letters = document.querySelectorAll(".scoreboard-letter");
 
-function init() {
+async function init() {
   let currentRow = 0;
   let currentGuess = "";
   let done = false;
+  let isShowingModal = true;
+
+  const res = await fetch(
+    "http://127.0.0.1:3000/get-word-of-the-day?random=true"
+  );
+  const { word: wordRes } = await res.json();
+  const word = wordRes.toUpperCase();
+  const wordParts = word.split("");
+  isShowingModal = false;
+  setLoading(isShowingModal);
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
@@ -31,11 +28,27 @@ function init() {
       letter;
   }
 
-  function commit() {
+  async function commit() {
     if (currentGuess.length !== ANSWER_LENGTH) {
       // do nothing
       return;
     }
+
+    isShowingModal = true;
+    setLoading(isShowingModal);
+    const res = await fetch("http://localhost:3000/validate-word", {
+      method: "POST",
+      body: JSON.stringify({ word: currentGuess }),
+    });
+    const { isValidWord } = await res.json();
+
+    if (!isValidWord) {
+      setLoading(false);
+      setInfo("Not a valid word", isShowingModal);
+      return;
+    }
+    isShowingModal = false;
+    setLoading(isShowingModal);
 
     const guessParts = currentGuess.split("");
     const map = makeMap(wordParts);
@@ -68,26 +81,24 @@ function init() {
     currentGuess = "";
     if (allRight) {
       // win
-      alert("you win");
+      isShowingModal = true;
+      setInfo("you win", isShowingModal);
       done = true;
     } else if (currentRow === ROUNDS) {
       // lose
-      alert(`you lose, the word was ${word}`);
+      isShowingModal = true;
+      setInfo(`you lose, the word was ${word}`, isShowingModal);
       done = true;
     }
   }
 
   function backspace() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
-    console.log(
-      currentGuess,
-      currentRow * ANSWER_LENGTH + currentGuess.length - 1
-    );
     letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = "";
   }
 
   document.addEventListener("keydown", function handleKeyPress(event) {
-    if (done) {
+    if (done || isShowingModal) {
       // do nothing;
       return;
     }
@@ -104,10 +115,31 @@ function init() {
       // do nothing
     }
   });
+
+  document
+    .querySelector(".info-button")
+    .addEventListener("click", function handleInfoButtonClick() {
+      isShowingModal = false;
+      setInfo("", isShowingModal);
+    });
 }
 
 function isLetter(letter) {
   return letter.length === 1 && letter.match(/[a-zA-Z]/) !== null;
+}
+
+const loadingDiv = document.querySelector(".loading");
+const infoDiv = document.querySelector(".info");
+const infoTextDiv = document.querySelector(".info-text");
+const spiralDiv = document.querySelector(".spiral");
+function setLoading(isShowingModal) {
+  loadingDiv.classList.toggle("hidden", !isShowingModal);
+  spiralDiv.classList.toggle("hidden", !isShowingModal);
+}
+function setInfo(text, isShowingModal) {
+  loadingDiv.classList.toggle("hidden", !isShowingModal);
+  infoDiv.classList.toggle("hidden", !isShowingModal);
+  infoTextDiv.innerText = text;
 }
 
 function makeMap(array) {
