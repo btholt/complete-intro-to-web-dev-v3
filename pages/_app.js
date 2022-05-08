218c477cb6684858a1c9491d26851751fe39643c
Brian Holt <btholt@gmail.com>
2022-05-07 18:39:29 -0700

    favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "@fortawesome/fontawesome-free/js/all.js";
+import Head from "next/head";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 import "highlight.js/styles/a11y-light.css";
 import "../styles/variables.css";
@@ -27,6 +28,36 @@ globalThis.klipse_settings = {
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${process.env.BASE_URL}/images/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${process.env.BASE_URL}/images/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${process.env.BASE_URL}/images/favicon-16x16.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${process.env.BASE_URL}/images/favicon-16x16.png`}
+        />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href={`${process.env.BASE_URL}/images/favicon.ico`}
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
